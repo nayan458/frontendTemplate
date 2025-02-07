d0cb28c33d0e932ba854546a0f58eb650557d339
Nayanmoni <nayancoding@gmial.com>
2025-02-07 13:55:19 +0530

    created a react-frontend template for typescript + swc

---
+++ src/routes/Demo.tsx
@@ -0,0 +1,14 @@
+
+
+// const LoginRoutes = {
+//   path: '/',
+//   element: // <Parent/>,
+//   children: [
+//     {
+//       path: '/demo',
+//       element: // < demo />,
+//     },
+//   ],
+// };
+
+// export default LoginRoutes;
---
+++ tailwind-config/theme/colors.js
@@ -0,0 +1,41 @@
+export default {
+  primary: {
+    DEFAULT: '#435334', // Primary green
+    dark: '#39462C', // Darker primary
+    light: '#9EB384', // Lighter primary
+    pale: '#CEDEBD', // Pale green
+  },
+  secondary: {
+    DEFAULT: '#ffffff', // White
+    dark: '#2D2D2D', // Dark gray
+    light: '#D9D9D9', // Light gray
+    text: {
+      DEFAULT: '#25293F', // Text gray
+      dark: '#1B2559', // Darker text
+    },
+  },
+  warning: {
+    light: '#FFEED6', // Light warning
+    DEFAULT: '#F69727', // Warning orange
+    dark: '#D6BC97', // Darker accent
+  },
+  notification: {
+    light: '#FAF1E4', // Notification background
+    DEFAULT: '#FFEED6', // Notification border
+  },
+  error: {
+    DEFAULT: '#E53E3E', // Standard red for errors
+    dark: '#9B2C2C', // Dark error red
+    light: '#FED7D7', // Light error red
+  },
+  success: {
+    DEFAULT: '#38A169', // Standard green for success
+    dark: '#276749', // Dark success green
+    light: '#C6F6D5', // Light success green
+  },
+  info: {
+    DEFAULT: '#3182CE', // Standard blue for information
+    dark: '#2A4365', // Dark info blue
+    light: '#BEE3F8', // Light info blue
+  },
+};
